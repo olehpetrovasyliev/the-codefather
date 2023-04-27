e0778f7a5a3dc6a594978fd75e7233b629ecc20c
MariiaVoloshanovska <masterskaiamary@gmail.com>
2023-04-27 22:15:16 +0300

    1

--- src/js/annimator.js
+++ src/js/annimator.js
@@ -1,23 +1,11 @@
 const elements = document.querySelectorAll('.my-element');
-
-elements.forEach(element => {
-  const observer = new IntersectionObserver(entries => {
-    if (entries[0].isIntersecting) {
-      element.classList.add('animate');
-    } else {
-      element.classList.remove('animate');
-    }
-  });
-  observer.observe(element);
-});
-
-const btnElement = document.querySelector('.catalog-button');
-
 const observer = new IntersectionObserver(entries => {
   if (entries[0].isIntersecting) {
-    btnElement.classList.add('heartbeat');
+    element.classList.add('animate');
   } else {
-    btnElement.classList.remove('heartbeat');
+    element.classList.remove('animate');
   }
 });
-observer.observe(btnElement);
+
+elements.forEz;
+observer.observe(element);
